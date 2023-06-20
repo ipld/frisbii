4a68317316b2c0df6e8de2ddd732892ce2a44a24
Rod Vagg <rod@vagg.org>
2023-06-20 16:27:23 +1000

    chore: use upstreamed go-libipni httpsync changes
    Custom changes here were merged upstream

--- httpsync/doc.go
+++
@@ -1,4 +0,0 @@
-// Package httpsync is a fork of github.com/ipni/go-libipni/dagsync/httpsync
-// with some modifications to make it work for the current Frisbii usecase. It
-// needs to be reconciled back into that package and removed from here.
-package httpsync
